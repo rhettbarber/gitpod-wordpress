b970133da294ce91f855eda21fc541ab626afe48
rhettbarber <b_rhettbarber@yahoo.com>
2021-02-28 23:59:59 +0000

    copied luiz files locally

---
+++ .init.sh
@@ -0,0 +1,7 @@
+# docs: https://developer.wordpress.org/cli/commands/plugin/
+
+wp plugin delete hello
+wp plugin delete akismet
+wp plugin activate ${REPO_NAME}
+
+# wp plugin install plugin_slug --activate
